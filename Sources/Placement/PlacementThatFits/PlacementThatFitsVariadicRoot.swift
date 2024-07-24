279fb515f16f2f7200c758bbbd725854d38dc063
Matthew Morgan <matthewm@apadmi.com>
2024-07-24 14:50:14 +0100

    Remove fixed size modifier on children of PlacementThatFits

--- Sources/Placement/PlacementThatFits/PlacementThatFitsVariadicRoot.swift
+++ Sources/Placement/PlacementThatFits/PlacementThatFitsVariadicRoot.swift
@@ -16,7 +16,6 @@ struct PlacementThatFitsVariadicRoot: _VariadicView_MultiViewRoot {
         ForEach(children) { child in
             let index = children.firstIndex { $0.id == child.id }
             child
-                .fixedSize()
                 .modifier(PlacementThatFitsChildModifier(index: index, coordinator: coordinator))
         }
     }
